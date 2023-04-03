e8a0398e54f102baaa90d50706b6ae063206ada6
MichaelPE16 <93220179+MichaelPE16@users.noreply.github.com>
2023-04-03 04:05:46 -0400

    Add files via upload

---
+++ ejercicios.js
@@ -0,0 +1,18 @@
+/*let H = "hola como estas "
+let resultado = H.repeat(3)
+document.write(resultado);*/
+
+/*// split dividir lo que le coloquemos en un array dividido entre comas en este caso
+let h = "Tu, nombre, es"
+result = h.split(",")
+document.write(result[0])*/
+
+/*//substring
+let h = "Tunombrees";
+result = h.substring(1,3)  
+document.write(result)*/
+
+//low y uppercase
+let h = "Tu nombre es";
+result = h.toUpperCase(); 
+document.write(result)
